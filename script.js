0e74942bf03390f229600f182cba021195610f4b
Vipul rai <raiv5993@gmail.com>
2025-09-20 14:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+const nameInput = document.getElementById('name');
+const yearInput = document.getElementById('year');
+const button = document.getElementById('button');
+const urlText = document.getElementById('url');
+
+button.addEventListener('click', () => {
+  const name = encodeURIComponent(nameInput.value.trim());
+  const year = encodeURIComponent(yearInput.value.trim());
+
+  
+  let query = '';
+  if (name) query += `name=${name}`;
+  if (year) query += (query ? '&' : '') + `year=${year}`;
+
+  urlText.textContent = query 
+    ? `https://localhost:8080/?${query}`
+    : `https://localhost:8080/`;
+});
